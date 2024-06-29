47f897604962de72b896198bc45ec46838e637af
Angel <palkina.sof@gmail.com>
2024-06-29 23:41:44 +0500

    fix bugs with picture

--- src/comic.ts
+++ src/comic.ts
@@ -1,20 +1,4 @@
 import { formatDistanceToNow } from "date-fns";
-main();
-
-
-type Comic = {
-    month: string;
-    num: number;
-    link: string;
-    year: string;
-    news: string;
-    safe_title: string;
-    transcript: string;
-    alt: string;
-    img: string;
-    title: string;
-    day: string;
-  };
 
 async function main() {
     // Получение ID через fetch
@@ -29,42 +13,36 @@ async function main() {
     const responseComic = await fetch(url);
     const info = await responseComic.json();
 
-    document.addEventListener('DOMContentLoaded', function() {
-        const imgElement = document.getElementById('comic-picture');
-        if (imgElement) {
-            //создание картинки и альтернативного текста
-            const img = document.createElement("img");
-            img.src = info.img;
-            img.alt = info.alt;
-            imgElement.appendChild(img); // добавление элемента по Id в документ
+    let comicPicture = document.getElementById("comic-picture") as HTMLElement;
 
-            // сохдание заголовка без использования innerHTML
-            const title = document.createElement("h2");
-            title.textContent = info.safe_title;
-            imgElement.appendChild(title);// добавление элемента по Id в документ
+    //создание картинки и альтернативного текста
+    const img = document.createElement("img");
+    img.src = info.img;
+    img.alt = info.alt;
+    comicPicture.appendChild(img); // добавление элемента по Id в документ
 
-            // создание даты
-            const date = new Date(
-                Date.UTC(
-                    Number.parseInt(info.year),
-                    Number.parseInt(info.month) - 1
-                )
-            );
+    // сохдание заголовка без использования innerHTML
+    const title = document.createElement("h2");
+    title.textContent = info.safe_title;
+    comicPicture.appendChild(title);// добавление элемента по Id в документ
 
-            const time = document.createElement("time");
-            time.innerHTML = date.toLocaleDateString(
-                "en",
-                { 
-                    month: "long",
-                    year: "numeric"
-                }
-            );
-            imgElement.appendChild(time);// добавление элемента по Id в документ
-        } else {
-            console.error("Элемент с ID 'comic-picture' не найден");
+    // создание даты
+    const date = new Date(
+        Date.UTC(
+            Number.parseInt(info.year),
+            Number.parseInt(info.month) - 1
+        )
+    );
+
+    const time = document.createElement("time");
+    time.innerHTML = date.toLocaleDateString(
+        "en",
+        { 
+            month: "long",
+            year: "numeric"
         }
-    });
-    
-
-    
+    );
+    comicPicture.appendChild(time);// добавление элемента по Id в документ
 }
+
+main();
